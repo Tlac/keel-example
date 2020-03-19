c75ae72f33d83b65fc8ec463b987eddbaf6b50f5
Tlac <thomaslac912@gmail.com>
2020-03-19 02:09:54 -0400

    Test auto build for docker-hub

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ var version = "v1"
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome to my website! Version %s", version)
+		fmt.Fprintf(w, "I updated the site!", version)
 	})
 	fmt.Printf("App is starting, version: %s \n", version)
 	log.Fatal(http.ListenAndServe(":8500", nil))
